454b1b2f793fedfb3cc27dbe4a13fd06fcfb045f
mbr84 <matthew.bramfeld@gmail.com>
2016-11-29 18:05:46 -0800

    questionable tactics

--- public/codeNav.js
+++ public/codeNav.js
@@ -1,34 +1,58 @@
 /* eslint-disable no-undef */
 
 $(document).ready(() => {
-  var codePanes = {
-    one: [$("div[data-screen='one'] + .code-pane .out")],
-    two: [$("div[data-screen='two'] + .code-pane .out")],
-    three: [$("div[data-screen='three'] + .code-pane .out")],
-    four: [$("div[data-screen='four'] + .code-pane .out")],
-    five: [$("div[data-screen='five'] + .code-pane .out")],
-    six: [$("div[data-screen='six'] + .code-pane .out")],
+  var activePanes = {
+    one: false,
+    two: false,
+    three: false,
+    four: false,
+    five: false,
+    six: false,
   }
   let zIndex = 2
 
   var showCode = (e) => {
     var code = $(`div[data-screen='${e.currentTarget.dataset.screen}'] + .code-pane div[data-lang='${e.target.dataset.lang}`);
-    var codePane = codePanes[e.currentTarget.dataset.screen]
-    if (code.hasClass('out')) {
-      var topItem = codePane[codePane.length - 1];
-      if (topItem.data('lang') !== (e.target.dataset.lang)) {
-        topItem.toggleClass('out');
-        codePane.pop()
-        setTimeout(() => { showCode(e); }, 300)
-      }
-    } else {
-      codePane.push(code)
-      code.css('z-index', ++zIndex);
+    var codePane = $(`div[data-screen='${e.currentTarget.dataset.screen}'] + .code-pane`);
+    var activePane = activePanes[e.currentTarget.dataset.screen]
+console.log(activePane)
+
+
+    if (!activePane) {
       code.toggleClass('out');
+      activePanes[e.currentTarget.dataset.screen] = code;;
+      codePane.toggleClass('up');
+    } else {
+      codePane.toggleClass('down');
+      if (!code.hasClass('out')) {
+        setTimeout(() => {
+          codePane.toggleClass('hide-transition swing');
+          setTimeout(() => {
+            activePane.toggleClass('out');
+            code.toggleClass('out');
+            activePanes[e.currentTarget.dataset.screen] = code;
+
+            codePane.toggleClass('down up');
+            codePane.toggleClass('hide-transition swing');
+            setTimeout(() => { codePane.toggleClass('up'); }, 25)
+          }, 25)
+        }, 500);
+      } else {
+        setTimeout(() => {
+          code.toggleClass('out');
+          activePanes[e.currentTarget.dataset.screen] = false;
+          setTimeout(() => {
+            codePane.toggleClass('hide-transition');
+            codePane.toggleClass('down');
+            setTimeout(() => codePane.toggleClass('hide-transition up'), 10)
+          }, 25)
+        }, 750)
+      }
     }
   }
 
   $('.menu-bar').click((e) => {
     showCode(e);
   })
+
 })
