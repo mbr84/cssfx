f37270be69c75edcb1b24c61ecb58a2fde244e55
mbr84 <matthew.bramfeld@gmail.com>
2017-02-07 00:31:45 -0800

    varies code-snip transition duration

--- public/codeNav.js
+++ public/codeNav.js
@@ -14,8 +14,8 @@ $(document).ready(() => {
     var newId = 'code-snip' + "_" + index
     $(this).attr('id', newId)
     if ($(this).height() > 300) {
-      $(`#${newId}`).css('transition', "transform 1s cubic-bezier(.24,1.61,.35,.89), -webkit-transform 1s cubic-bezier(.24,1.61,.35,.89)")
-    } else if ($(this).height() > 200 && $(this).height() < 300) {
+      $(`#${newId}`).css('transition', "transform .95s cubic-bezier(.24,1.61,.35,.89), -webkit-transform .95s cubic-bezier(.24,1.61,.35,.89)")
+    } else if ($(this).height() > 200) {
       $(`#${newId}`).css("transition", "transform .85s cubic-bezier(.24,1.61,.35,.89), -webkit-transform .85s cubic-bezier(.24,1.61,.35,.89)")
     }
   })
