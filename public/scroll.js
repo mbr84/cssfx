972f9bafcc41049bd9ac8c65f43f95564851a34a
mbr84 <matthew.bramfeld@gmail.com>
2017-03-13 13:48:57 -0700

    moves distinctUntilChanged

--- public/scroll.js
+++ public/scroll.js
@@ -27,7 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
       // Get the number of screens between the screen we're navigating to
       // and the screen we're currently on
 
-      .map(click => indexOf(click) - activeNow()),
+      .map(click => indexOf(click) - activeNow())
+      .distinctUntilChanged(), // Ignore click if screen is already active 
     keyScrolls.filter(key => key.which === 40 || key.which === 38)
 
       // Up and down arrows move us up or down by one screen.
@@ -45,7 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
     )
     .scan((currentScreen, screensToTraverse) => currentScreen + screensToTraverse, 0) //start on 0th screen
     .filter(screenIdx => screenIdx >= 0 && screenIdx <= 6)
-    .distinctUntilChanged()
 
   screens.forEach(screenIdx => {
     updateMenu(screenIdx);
