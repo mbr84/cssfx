6f4b756cb442728e5d595f3c49b24bfd119465b1
mbr84 <matthew.bramfeld@gmail.com>
2017-03-24 19:00:01 -0700

    transitionEnd stream

--- src/scroll.js
+++ src/scroll.js
@@ -1,59 +1,64 @@
 $(document).ready(()  => {
-  const wheels$ = Rx.Observable.fromEvent(document, 'wheel');
-  const keyDowns$ = Rx.Observable.fromEvent(document, 'keydown');
-  const menuClicks$ = Rx.Observable.fromEvent($('.contents'), 'click');
-  const mobileButtons$ = Rx.Observable.fromEvent($('.mobile-buttons'), 'click touch');
+  const wheels = Rx.Observable.fromEvent(document, 'wheel');
+  const keyDowns = Rx.Observable.fromEvent(document, 'keydown');
+  const menuClicks = Rx.Observable.fromEvent($('[data-position]'), 'click');
+  const mobileButtons = Rx.Observable.fromEvent($('.mobile-buttons'), 'click touch');
+  const transitions = Rx.Observable.fromEvent($('.left-scroll, .right-scroll'), 'transitionend')
 
   const navBtns = Array.from($('[data-position]'));
   const indexOf = (e) => Number(e.target.dataset.position)
   const activeIdx = () => navBtns.reduce((active, el, i) => el.classList.contains('active') ? i : active, 0)
-  const updateMenu = (idx) => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
 
-  const scroll = (screenIdx) => {
-    $('.left-scroll').css({ transform: `translateY(${-100 * screenIdx}vh)` });
-    $('.right-scroll').css({ transform: `translateY(${100 * screenIdx}vh)` });
+  const leftScroll = document.querySelector('.left-scroll')
+  const rightScroll = document.querySelector('.right-scroll')
+
+  const updateMenu = (idx) => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
+  const scroll = (nextScreenIdx) => {
+    leftScroll.classList.remove(`screen${activeIdx()}`)
+    rightScroll.classList.remove(`screen${activeIdx()}`)
+    leftScroll.classList.add(`screen${nextScreenIdx}`)
+    rightScroll.classList.add(`screen${nextScreenIdx}`)
+  }
+
+  const onTransitionEnd = () => {
+    leftScroll.classList.remove('ready--scroll', 'end-to-end' )
+    rightScroll.classList.remove('ready--scroll', 'end-to-end' )
   }
 
   const bounceBack = () => {
     $('.left-scroll, .right-scroll').toggleClass('end-to-end')
-    setTimeout(() => $('.left-scroll, .right-scroll').toggleClass('end-to-end'), 100)
+  }
+
+  const readyScroll = () => {
+    leftScroll.classList.add('ready--scroll')
+    rightScroll.classList.add('ready--scroll')
   }
 
   const nextScreen = (screen) => {
-    if (screen < 0 || screen > 6) bounceBack()
+    screen < 0 || screen > 6 ? bounceBack() : readyScroll()
     return (screen + 7) % 7  // -1 => 6, 7 => 0
   }
 
-  // Map menuClicks, keyscrolls, and wheels to computation fn's, merge them
-  //  into a single stream, then scan them to get the stream of active screens
-
-  const arrowDeltas$ = keyDowns$
-    .filter(key => key.which === 40 || key.which === 38)
-    .map(key => key.which === 40 ? 1 : -1)
-
-  const mobileButtonDeltas$ = mobileButtons$
-    .map(e => e.target.innerText === "Next" ? 1 : -1)
-
-  const wheelDeltas$ = wheels$
-    .filter(wheel => Math.abs(wheel.deltaY) > 75) // Ignore tiny, lingering mousewheel events
-    .throttleTime(700)
-    .map(wheel => wheel.deltaY / Math.abs(wheel.deltaY))
-
-  const scrollActions = Rx.Observable.merge(arrowDeltas$, wheelDeltas$, mobileButtonDeltas$)
+  // Map keydowns, mobile button clicks, mousewheels, and menu clicks to computation fn's,
+  // merge them into a single stream, then scan them to get the stream of focused screens
+  const screens = keyDowns.filter(key => key.which === 40 || key.which === 38)
+    .map(key => key.which === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see lines 30 & 33)
+    .merge(
+      mobileButtons.map(e => e.target.innerText === "Next" ? 1 : -1),
+      wheels.filter(wheel => Math.abs(wheel.deltaY) > 75) // Ignore lingering mousewheel events
+        .throttleTime(700)
+        .map(wheel => wheel.deltaY / Math.abs(wheel.deltaY))
+    )
     .map(delta => currentScreen => nextScreen(currentScreen + delta))
-
-  const clickActions = menuClicks$
-    .filter(click => click.target.tagName === 'LI')
-    .map(click => (__) => indexOf(click))
-
-  const screens = Rx.Observable.merge(clickActions, scrollActions)
+    .merge(menuClicks.map(click => (__) => indexOf(click)))
     .startWith(0)
     .scan((acc, curr) => curr(acc))
     .distinctUntilChanged()
 
+  transitions.forEach(onTransitionEnd)
   screens.forEach(screenIdx => {
-    updateMenu(screenIdx);
     scroll(screenIdx);
+    updateMenu(screenIdx);
   })
 
 // Stop scroll propagation on scrollable child elements. (In our case they're all <pre>'s)
