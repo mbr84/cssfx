9c24863a015df11f2b736fc143c4bdb419b5b9df
mbr84 <matthew.bramfeld@gmail.com>
2017-03-26 01:06:09 -0700

    fix-choppy-scrolling

--- src/scroll.js
+++ src/scroll.js
@@ -1,72 +1,59 @@
-$(document).ready(()  => {
-  const wheels = Rx.Observable.fromEvent(document, 'wheel');
-  const keyDowns = Rx.Observable.fromEvent(document, 'keydown');
-  const menuClicks = Rx.Observable.fromEvent($('[data-position]'), 'click');
-  const mobileButtons = Rx.Observable.fromEvent($('.mobile-buttons'), 'click touch');
-  const transitions = Rx.Observable.fromEvent($('.left-scroll, .right-scroll'), 'transitionend')
+$(document).ready(() => {
+  var wheels      = Rx.Observable.fromEvent(document, 'wheel');
+  var keyScrolls  = Rx.Observable.fromEvent(document, 'keydown');
+  var menuClicks  = Rx.Observable.fromEvent($('.contents'), 'click');
 
-  const navBtns = Array.from($('[data-position]'));
-  const activeIdx = () => navBtns.reduce((active, el, i) => el.classList.contains('active') ? i : active, 0)
-  const goToIndexOf = e => {
-    readyScroll()
-    return Number(e.target.dataset.position)
+  // Each nav button has an HTML attribute 'data-position.' Anything
+  // with a 'data position' attribute is a button on the nav bar
+
+  var navBtns     = Array.from($('[data-position]'));
+
+  // Each button's 'data-position' attribute gives the 'index' of the screen it links to
+
+  var indexOf     = (e) => e.target.dataset.position
+  var activeIdx   = () => navBtns.reduce((active, el, i) => el.classList.contains('active') ? i : active, 0)
+  var updateMenu  = (idx) => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
+
+  var scroll      = (idx) => {
+    $('.left-scroll').css({ transform: `translateY(${-100 * idx}vh)` });
+    $('.right-scroll').css({ transform: `translateY(${100 * idx}vh)` });
   }
 
-  const leftScroll = document.querySelector('.left-scroll')
-  const rightScroll = document.querySelector('.right-scroll')
+  // Merge menuClicks, keyscrolls, and wheels into a single stream
 
-  const updateMenu = idx => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
-  const scroll = (nextScreenIdx) => {
-    leftScroll.classList.remove(`screen${activeIdx()}`)
-    rightScroll.classList.remove(`screen${activeIdx()}`)
-    leftScroll.classList.add(`screen${nextScreenIdx}`)
-    rightScroll.classList.add(`screen${nextScreenIdx}`)
-  }
+  var screens     = Rx.Observable.merge(
+    menuClicks.filter(click => click.target.tagName === 'LI')
 
-  const onTransitionEnd = () => {
-    leftScroll.classList.remove('ready--scroll', 'end-to-end' )
-    rightScroll.classList.remove('ready--scroll', 'end-to-end' )
-  }
+  // Get the number of screens between the screen we're navigating to
+  // and the screen we're currently on
 
-  const bounceBack = () => {
-    $('.left-scroll, .right-scroll').toggleClass('end-to-end')
-  }
+      .map(clickedButton => indexOf(clickedButton) - activeIdx()),
+    keyScrolls.filter(key => key.which === 40 || key.which === 38)
 
-  const readyScroll = () => {
-    leftScroll.classList.add('ready--scroll')
-    rightScroll.classList.add('ready--scroll')
-  }
+  // Up and down arrows move us up or down by one screen.
+  // Wheel events do the same (line 43).
 
-  const nextScreen = (screen) => {
-    screen < 0 || screen > 6 ? bounceBack() : readyScroll()
-    return (screen + 7) % 7  // -1 => 6, 7 => 0
-  }
+      .map(key => key.which === 40 ? 1 : -1),
 
-  // Map keydowns, mobile button clicks, mousewheels, and menu clicks to computation fn's,
-  // merge them into a single stream, then scan them to get the stream of focused screens
-  const screens = keyDowns.filter(key => key.which === 40 || key.which === 38)
-    .map(key => key.which === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see lines 30 & 33)
-    .merge(
-      mobileButtons.map(e => e.target.innerText === "Next" ? 1 : -1),
-      wheels.filter(wheel => Math.abs(wheel.deltaY) > 75) // Ignore lingering mousewheel events
-        .throttleTime(700)
-        .map(wheel => wheel.deltaY / Math.abs(wheel.deltaY))
+  // Ignore lingering mousewheel events with low deltaY properties, so we
+  // can set throttleTime as low as possible and keep the page responsive
+
+    wheels.filter(wheel => Math.abs(wheel.deltaY) > 75)
+      .throttleTime(700)
+      .map(wheel => wheel.deltaY / Math.abs(wheel.deltaY))
     )
-    .map(delta => currentScreen => nextScreen(currentScreen + delta))
-    .merge(menuClicks.map(click => (__) => goToIndexOf(click)))
-    .startWith(0)
-    .scan((acc, curr) => curr(acc))
-    .distinctUntilChanged()
+    .scan((currentScreen, screensToTraverse) => currentScreen + screensToTraverse, 0)
+    .distinctUntilChanged() // Ignore click if screen is already active
+    .filter(screenIdx => screenIdx >= 0 && screenIdx <= 6)
 
-  transitions.forEach(onTransitionEnd)
   screens.forEach(screenIdx => {
-    scroll(screenIdx);
     updateMenu(screenIdx);
+    scroll(screenIdx);
   })
 
-// Stop scroll propagation on scrollable child elements. (In our case they're all <pre>'s)
+  // Stop scroll propagation on scrollable child elements. (In our case they're all <pre>'s)
 
-  $('pre').on('wheel', e => {
+  $('pre').on('wheel', function (e) {
     e.stopPropagation();
   });
 })
