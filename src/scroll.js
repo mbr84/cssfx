13c748da7c864496ea068801002219929f62d839
mbr84 <matthew.bramfeld@gmail.com>
2017-04-03 21:39:52 -0700

    removed extra code and empty lines and reordered some things

--- src/scroll.js
+++ src/scroll.js
@@ -1,34 +1,28 @@
 $(document).ready(()  => {
-  const wheels = Rx.Observable.fromEvent(document, 'wheel');
   const keyDowns = Rx.Observable.fromEvent(document, 'keydown');
   const menuClicks = Rx.Observable.fromEvent($('[data-position]'), 'click');
   const mobileButtons = Rx.Observable.fromEvent($('.mobile-buttons'), 'click touch');
+  const wheels = Rx.Observable.fromEvent(document, 'wheel');
+  $('pre').on('wheel', e => e.stopPropagation())
 
   const panes = $('.left-scroll, .right-scroll')
   const navBtns = Array.from($('[data-position]'));
-
-  const updateMenu = idx => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
-  var scroll      = (idx) => {
+  const scroll = (idx) => {
     $('.left-scroll').css({ transform: `translateY(${-100 * idx}vh)` });
     $('.right-scroll').css({ transform: `translateY(${100 * idx}vh)` });
   }
-
+  const updateMenu = idx => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
   const updateScrollClasses = (screen) => {
-    if (screen < 0 || screen > 6) {
-      panes.removeClass('ready-scroll').addClass('end-to-end')
-    } else {
-      panes.removeClass('end-to-end').addClass('ready-scroll')
-    }
+    screen < 0 || screen > 6 ? panes.addClass('end-to-end') : panes.removeClass('end-to-end')
     return screen
   }
-
   const nextScreen = screen => (screen + 7) % 7  // -1 => 6, 7 => 0
   const indexOfClick = e => Number(e.target.dataset.position)
 
   // Map keydowns, mobile button clicks, mousewheels, and menu clicks to computation fn's,
   // merge them into a single stream, then scan them to get the stream of focused screens
   const screens = keyDowns.filter(key => key.which === 40 || key.which === 38)
-    .map(key => key.which === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see line 36)
+    .map(key => key.which === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see line 30)
     .merge(
       mobileButtons.map(e => e.target.innerText === "Next" ? 1 : -1),
       wheels.filter(wheel => Math.abs(wheel.deltaY) > 75) // Ignore lingering mousewheel events
@@ -43,10 +37,4 @@ $(document).ready(()  => {
     scroll(screenIdx);
     updateMenu(screenIdx);
   })
-
-   // Stop scroll propagation on scrollable child elements. (In our case they're all <pre>'s)
-
-  $('pre').on('wheel', function (e) {
-    e.stopPropagation();
-  });
 })
