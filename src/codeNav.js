60f4325a125e8bbd4ee158fb69025df89d7a99d4
mbr84 <matthew.bramfeld@gmail.com>
2017-04-18 12:18:34 -0700

    adds fix for annoying firefox 52 breaking change

--- src/codeNav.js
+++ src/codeNav.js
@@ -1,6 +1,16 @@
 $(document).ready(() => {
   const codeButtonClicks = Rx.Observable.fromEvent($('.code-button'), 'click');
   const animationEnds = Rx.Observable.fromEvent($('.code-snip'), 'animationend');
+  const isFirefox = typeof InstallTrigger !== 'undefined';
+  if (isFirefox) {
+    Rx.Observable.fromEvent($('.code-snip'), 'transitionend')
+      .map(e => $(`#code-snip-${getIdNum(e)}`))
+      .forEach(codeSnip => {
+        codeSnip.css('display', 'none')
+        codeSnip.outerHeight();
+        codeSnip.css('display', '')
+      })
+  }
 
   const getIdNum = e => e.target.id.split('-')[2]
   const updateTransitionClasses = el  => {
