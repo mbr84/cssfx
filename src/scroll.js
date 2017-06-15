742c5892df4ee108598ee80674f0d3688be15e17
mbr84 <matthew.bramfeld@gmail.com>
2017-06-15 13:28:13 -0700

    cleaned up scrollpage

--- src/scroll.js
+++ src/scroll.js
@@ -11,26 +11,38 @@ $(document).ready(()  => {
     $('.left-scroll').css({ transform: `translateY(${-100 * idx}vh)` });
     $('.right-scroll').css({ transform: `translateY(${100 * idx}vh)` });
   }
-  const updateMenu = idx => $([navBtns[idx]]).addClass('active').siblings().removeClass('active')
+  const updateMenu = idx =>
+    $([navBtns[idx]]).addClass('active')
+      .siblings()
+      .removeClass('active')
+
   const updateScrollClasses = (screen) => {
     screen < 0 || screen > 6 ? panes.addClass('end-to-end') : panes.removeClass('end-to-end')
     return screen
   }
+
   const nextScreen = screen => (screen + 7) % 7  // -1 => 6, 7 => 0
   const indexOfClick = e => Number(e.target.dataset.position)
 
   // Map keydowns, mobile button clicks, mousewheels, and menu clicks to computation fn's,
   // merge them into a single stream, then scan them to get the stream of focused screens
-  const screens = keyDowns.filter(key => key.which === 40 || key.which === 38)
-    .map(key => key.which === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see line 30)
-    .merge(
-      mobileButtons.map(e => e.target.innerText === "Next" ? 1 : -1),
-      wheels.filter(wheel => Math.abs(wheel.deltaY) > 75) // Ignore lingering mousewheel events
-        .throttleTime(700)
-        .map(wheel => wheel.deltaY / Math.abs(wheel.deltaY))
-    )
-    .map(delta => fn => currentScreen => nextScreen(fn(currentScreen + delta)))
-    .merge(menuClicks.map(click => fn => (__) => indexOfClick(fn(click))))
+  const keyScrolls = keyDowns.pluck("which")
+    .filter(key => key === 40 || key === 38)
+    .map(key => key === 40 ? 1 : -1)  // keypress moves us -/+ 1 screen (see line 30)
+  const mobileScrolls = mobileButtons.map(e => e.target.innerText === "Next" ? 1 : -1)
+
+  const wheelScrolls = wheels.pluck("deltaY")
+    .filter(dy => Math.abs(dy) > 75) // Ignore lingering mousewheel events
+    .throttleTime(700)
+    .map(dy => dy / Math.abs(dy))
+
+  const menuClickActions = menuClicks.map(click => fn => (__) => indexOfClick(fn(click)))
+
+  const scrollActions = Rx.Observable
+    .merge( keyScrolls, mobileScrolls, wheelScrolls)
+      .map(delta => fn => currentScreen => nextScreen(fn(currentScreen + delta)))
+
+  scrollActions.merge(menuClickActions)
     .scan((acc, curr) => curr(updateScrollClasses)(acc), 0)
 
   screens.forEach(screenIdx => {
